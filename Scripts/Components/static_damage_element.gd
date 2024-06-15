9c3040fa8251439111a875c001c4e3844640dbba
MutantGinga <haydenwilliamhefford@yahoo.com>
2024-06-15 23:34:33 +0100

    Damage Element and Static Damage Element complete
    Worked on bounce behaviour as a group, changed from adding velocity to setting velocity.
    
    Added bounce_force as export variable, as this is the set velocity.
    
    If vector is zero (edge case), it gets difference in locations to find direction.
    
    Also changed layers for walls to make sure the player collides with them.

--- Scripts/Components/static_damage_element.gd
+++ Scripts/Components/static_damage_element.gd
@@ -1,6 +1,7 @@
 extends StaticBody2D
 
 @export var damage_amount: int = 2
+@export var bounce_force: float = 1000
 @onready var health_component
 
 # Called when the node enters the scene tree for the first time.
@@ -17,10 +18,9 @@ func _on_body_entered(body):
 	health_component = body.get_node("HealthComponent")
 	health_component.remove_health(damage_amount)
 	var direction = body.velocity.normalized()
-	print(str(direction * -1))
-	if direction.x != 0:
-		direction.x *= -1
-		body.velocity.x += 1000 * direction.x
-	if direction.y != 0:
-		direction.y *= -1
-		body.velocity.y += 1000 * direction.y
+	if direction == Vector2.ZERO:
+		direction = global_position - body.global_position
+		direction = direction.normalized()
+	print(direction)
+	
+	body.velocity = -direction * bounce_force
