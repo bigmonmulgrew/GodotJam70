5c745a0d09e5284cddcefb907bb016b20e14f273
Lee-Roy <ltd1999@hotmail.co.uk>
2024-06-15 15:21:57 +0100

    Updated TestFile

--- Scripts/Character/Character.gd
+++ Scripts/Character/Character.gd
@@ -1,5 +1,10 @@
 extends CharacterBody2D
 
+@export var health: int = 10
+
+
+func ready():
+	$HealthComponent.max_health = health
 
 func _physics_process(delta):
 	look_at(get_global_mouse_position())
