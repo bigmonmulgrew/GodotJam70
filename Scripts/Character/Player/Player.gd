c77dd0b1551ef5fdc87be5f7e3eeb4a9cbf6f496
Lee-Roy <ltd1999@hotmail.co.uk>
2024-06-16 12:57:03 +0100

    Changed action bindings to make sense, changed player action type to Node2D
    Changing the primaryaction and secondary action node to node2d preserves its location relative to the player.

--- Scripts/Character/Player/Player.gd
+++ Scripts/Character/Player/Player.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 # Gets the child of PrimaryAction node with index 0
 @onready var primary_action: Node2D = $PrimaryAction.get_child(0)
 # Gets the child of SecondaryAction node with index 0
-@onready var secondary_action: Node2D = $PrimaryAction.get_child(0)
+@onready var secondary_action: Node2D = $SecondaryAction.get_child(0)
 
 func _process(delta):
 	_check_input()
@@ -13,5 +13,11 @@ func _physics_process(delta):
 	move_and_slide()
 
 func _check_input():
+	# Check if primary action is null
+	if not primary_action:
+		return
+	# Check if secondary action is null
+	if not secondary_action:
+		return
 	if Input.is_action_just_pressed("primary_action"): primary_action.use()
 	if Input.is_action_just_pressed("secondary_action"): secondary_action.use()
--- Scripts/DELETEMETEST.gd
+++ Scripts/DELETEMETEST.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-@onready var player: CharacterBody2D = $Character
+@onready var player: CharacterBody2D = $KingArthur
 @onready var health_bar: TextureProgressBar = $CanvasLayer/HealthBar
 @onready var resource_bar: TextureProgressBar = $CanvasLayer/ResourceBar
 
@@ -11,9 +11,9 @@ func _ready():
 func _process(delta):
 	# Update Healthbar
 	health_bar.value = player.get_node("HealthComponent").health
-	if Input.is_action_just_pressed("right_mouse_button"):
+	if Input.is_action_just_pressed("primary_action"):
 		player.get_node("HealthComponent").add_health(10)
-	if Input.is_action_just_pressed("left_mouse_button"):
+	if Input.is_action_just_pressed("secondary_action"):
 		player.get_node("HealthComponent").remove_health(10)
 
 	# Update Resourcebar
