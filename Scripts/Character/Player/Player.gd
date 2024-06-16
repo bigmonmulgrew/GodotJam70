b95de55cedbc32a9fbfa75b3fa2c777989e96eca
Lee-Roy <ltd1999@hotmail.co.uk>
2024-06-16 14:39:06 +0100

    Reworked dash to be a defensive action
    It has a use method now

--- Scripts/Character/Player/Player.gd
+++ Scripts/Character/Player/Player.gd
@@ -4,6 +4,8 @@ extends CharacterBody2D
 @onready var primary_action: Node2D = $PrimaryAction.get_child(0)
 # Gets the child of SecondaryAction node with index 0
 @onready var secondary_action: Node2D = $SecondaryAction.get_child(0)
+# Gets the child of DefensiveAction node with index 0
+@onready var defensive_action: Node = $DefensiveAction.get_child(0)
 
 func _process(delta):
 	_check_input()
@@ -13,11 +15,14 @@ func _physics_process(delta):
 	move_and_slide()
 
 func _check_input():
-	# Check if primary action is null
+	# Check if primary action is not null
 	if primary_action:
 		if Input.is_action_just_pressed("primary_action"): primary_action.use()
-	# Check if secondary action is null
+	# Check if secondary action is not null
 	if secondary_action:
 		if Input.is_action_just_pressed("secondary_action"): secondary_action.use()
+	# Check if defensive action is not null
+	if defensive_action:
+		if Input.is_action_just_pressed("defensive_action"): defensive_action.use()
 	
 	
