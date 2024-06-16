34a32e949bffe9056416118dac2106d0810c9a4e
MutantGinga <haydenwilliamhefford@yahoo.com>
2024-06-16 16:29:54 +0100

    Started setting up GameManager scene
    GameManager is an autoload scene that allows data to persist between scenes in Godot. It currently holds values for the selected characters, their health values, and the current round number.
    
    The lists of all characters and unlocked characters will be condensed into a dictionary.
    
    Started experimenting with generating our own documentation in Godot's Help menu.

---
+++ Scripts/game_manager.gd
@@ -0,0 +1,41 @@
+extends Node
+
+var selected_characters
+var active_character_index: int = 0
+var character_health_values: Array[int]
+var round_number: int = 0
+var all_characters = ["char_1", "char_2", "char_3", "char_4", "char_5"]
+var unlocked_characters = [true, true, true, false, false]
+
+func _ready():
+	print("GameManage _ready() called")
+	call_deferred("_find_characters")
+	call_deferred("increment_round_counter")
+	update_active_character(1)
+
+## Updates currently active character when you swap character.
+##
+## This method takes in an integer, which represents the index of the newly-active character.
+##
+## @tutorial: update_active_character(1)
+## @tutorial: this would update the active character to index 1, which is the second character in the list.
+func update_active_character(index):
+	active_character_index = index
+	
+func display_unlocked_characters():
+	for i in range(unlocked_characters.size()):
+		if unlocked_characters[i]:
+			print(all_characters[i])
+#
+func _find_characters():
+	selected_characters = get_tree().get_nodes_in_group("players")
+	for character in selected_characters:
+		print(character)
+
+func update_health():
+	for i in range(selected_characters.size()):
+		character_health_values[i] = selected_characters[i].get_node("HealthComponent").health
+
+func increment_round_counter():
+	round_number += 1
+	
