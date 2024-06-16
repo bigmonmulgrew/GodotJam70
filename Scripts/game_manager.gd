84d54531661e318ea6d6fffb56ee09c57fe0f4dc
MutantGinga <haydenwilliamhefford@yahoo.com>
2024-06-16 17:17:49 +0100

    Added character_dict and end_round() method
    character_dict used as a better way to organise list of characters and their unlocked status.
    
    end_round method created to call at the end of round, it heals all unused characters, sets has_been_used to all false, and then updates the health values stored in character_health_values array

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -1,18 +1,16 @@
 extends Node
 class_name Game_Manager
 
-var selected_characters
+var selected_characters: Array[Node]
 var active_character_index: int = 0
+var has_been_used: Array[bool] = [false, false, false]
 var character_health_values: Array[int]
 var round_number: int = 0
-var all_characters = ["char_1", "char_2", "char_3", "char_4", "char_5"]
-var unlocked_characters = [true, true, true, false, false]
+var character_dict = {"char_1": true, "char_2": true, "char_3": true, "char_4": false, "char_5": false}
 
 func _ready():
-	print("GameManage _ready() called")
+	display_unlocked_characters()
 	call_deferred("_find_characters")
-	call_deferred("increment_round_counter")
-	update_active_character(1)
 
 ## Updates currently active character when you swap character.
 ##[br]
@@ -25,16 +23,18 @@ func _ready():
 ## This would update the active character to index 1, which is the second character in the list.
 func update_active_character(index: int):
 	active_character_index = index
+	has_been_used[index] = true
 	
 func display_unlocked_characters():
-	for i in range(unlocked_characters.size()):
-		if unlocked_characters[i]:
-			print(all_characters[i])
-#
+	for character in character_dict:
+		var unlocked = character_dict[character]
+		print("Character: " + str(character) + ", Status: " + str(unlocked))
+
 func _find_characters():
 	selected_characters = get_tree().get_nodes_in_group("players")
 	for character in selected_characters:
 		print(character)
+		print(character.get_node("HealthComponent").health)
 
 func update_health():
 	for i in range(selected_characters.size()):
@@ -42,4 +42,10 @@ func update_health():
 
 func increment_round_counter():
 	round_number += 1
-	
+
+func end_round():
+	for i in range(selected_characters.size()):
+		if has_been_used[i] == false:
+			selected_characters[i].get_node("HealthComponent").add_health(50)
+	has_been_used = [false, false, false]
+	update_health()
