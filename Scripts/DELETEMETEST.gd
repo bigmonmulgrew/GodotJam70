5aab6c7c718294efa82d8133a9a8d626308c934d
David Mulgrew <bigmonmulgrew@users.noreply.github.com>
2024-06-17 12:48:04 +0100

    fixed game loop merge issues

--- Scripts/DELETEMETEST.gd
+++ Scripts/DELETEMETEST.gd
@@ -4,10 +4,8 @@ extends Node2D
 @export_file("*.tscn") var target_Level_next
 
 @onready var player: CharacterBody2D = $KingArthur
-@onready var health_bar: TextureProgressBar = $CanvasLayer/HealthBar
+@onready var health_bar_ui: TextureProgressBar = $CanvasLayer/HealthBar
 @onready var resource_bar: TextureProgressBar = $CanvasLayer/ResourceBar
-
-
 
 func _ready():
 	health_bar_ui.player_max_health = player.get_node("HealthComponent").max_health
