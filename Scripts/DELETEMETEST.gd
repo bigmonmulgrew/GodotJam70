4c2ef37254326b1daafb72ab71ecfe4aaa91eeac
David Mulgrew <bigmonmulgrew@users.noreply.github.com>
2024-06-17 13:02:49 +0100

    fixed debug scene

--- Scripts/DELETEMETEST.gd
+++ Scripts/DELETEMETEST.gd
@@ -1,11 +1,8 @@
 extends Node2D
 
-
 @export_file("*.tscn") var target_Level_next
-
 @onready var player: CharacterBody2D = $KingArthur
-@onready var health_bar_ui: TextureProgressBar = $CanvasLayer/HealthBar
-@onready var resource_bar: TextureProgressBar = $CanvasLayer/ResourceBar
+@onready var health_bar_ui = $HealthBarUI
 
 func _ready():
 	health_bar_ui.player_max_health = player.get_node("HealthComponent").max_health
@@ -19,18 +16,14 @@ func _process(delta):
 
 func delete_me1():
 	# Use this to place code to not interfear with other debug code
+	
 	# Update Healthbar
-
 	health_bar_ui.player_current_health = player.get_node("HealthComponent").health
 	if Input.is_action_just_pressed("right_mouse_button"):
-
 		player.get_node("HealthComponent").add_health(10)
+		
 	if Input.is_action_just_pressed("secondary_action"):
 		player.get_node("HealthComponent").remove_health(10)
-
-
-	# Update Resourcebar
-	resource_bar.value = player.get_node("ResourceComponent").current_resource
 	
 func delete_me2():
 	# Use this to place code to not interfear with other debug code
