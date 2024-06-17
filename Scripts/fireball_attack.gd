65fbc36a7a100d948e4d4f7181de36ce437eb3c2
MutantGinga <haydenwilliamhefford@yahoo.com>
2024-06-18 00:36:28 +0100

    Fireball Attack
    Added fireball attack and swapped King Arthur's attack type to fireball_attack rather than sword swipe for testing

--- Scripts/fireball_attack.gd
+++ Scripts/fireball_attack.gd
@@ -1,1 +1,23 @@
 extends Attack
+
+
+var parent_component: CharacterBody2D
+var health_component: Node
+var can_fire = true
+
+var fireball_scene = preload("res://Scenes/fireball.tscn")
+
+func _ready():
+	parent_component = owner
+	health_component = owner.get_node("HealthComponent")
+
+func use():
+	fire()
+
+func fire():
+	if not can_fire:
+		return
+	var fireball_instance = fireball_scene.instantiate()
+	fireball_instance.projectile_direction = get_parent().rotation
+	fireball_instance.global_position = global_position
+	fireball_instance.global_position.x += 50
