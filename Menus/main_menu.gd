1c443fa3cc926d83f347110b0bdfc5f439308e8b
David Mulgrew <bigmonmulgrew@users.noreply.github.com>
2024-06-17 09:17:40 +0100

    Added menus for game loop

---
+++ Menus/main_menu.gd
@@ -0,0 +1,30 @@
+extends Control
+
+@export_file("*.tscn") var target_Level_start_game
+@export_file("*.tscn") var target_Level_credits
+@export_file("*.tscn") var target_Level_options
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("ui_cancel"):
+		_quit_game()
+
+func _on_start_button_pressed() -> void:
+	# Transition to the game scene
+	get_tree().change_scene_to_file(target_Level_start_game)
+
+func _on_quit_button_pressed() -> void:
+	_quit_game()
+	
+func _quit_game():
+	get_tree().quit()
+
+func _on_credits_button_pressed() -> void:
+	get_tree().change_scene_to_file(target_Level_credits)
+
+func _on_options_button_pressed() -> void:
+	get_tree().change_scene_to_file(target_Level_options)
--- Scripts/DELETEMETEST.gd
+++ Scripts/DELETEMETEST.gd
@@ -1,14 +1,24 @@
 extends Node2D
 
+@export_file("*.tscn") var target_Level_next
+
 @onready var player: CharacterBody2D = $KingArthur
 @onready var health_bar: TextureProgressBar = $CanvasLayer/HealthBar
 @onready var resource_bar: TextureProgressBar = $CanvasLayer/ResourceBar
+
 
 func _ready():
 	health_bar.max_value = player.get_node("HealthComponent").max_health
 	resource_bar.max_value = player.get_node("ResourceComponent").resource_cap
 
 func _process(delta):
+	delete_me1()
+	delete_me2()
+	delete_me3()
+	delete_me4()
+
+func delete_me1():
+	# Use this to place code to not interfear with other debug code
 	# Update Healthbar
 	health_bar.value = player.get_node("HealthComponent").health
 	if Input.is_action_just_pressed("primary_action"):
@@ -18,3 +28,18 @@ func _process(delta):
 
 	# Update Resourcebar
 	resource_bar.value = player.get_node("ResourceComponent").current_resource
+	
+func delete_me2():
+	# Use this to place code to not interfear with other debug code
+	pass
+	
+func delete_me3():
+	# Use this to place code to not interfear with other debug code
+	pass
+	
+func delete_me4():
+	# Use this to place code to not interfear with other debug code
+	# Dave doing debugging here
+	if Input.is_action_just_pressed("ui_text_completion_query"):
+		get_tree().change_scene_to_file(target_Level_next)
+	
