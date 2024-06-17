82cccb0ed38bfc81c6a772ba8541883d62510555
Lee-Roy <ltd1999@hotmail.co.uk>
2024-06-17 15:56:31 +0100

    Automatically adds the timer to use.

--- Scripts/Components/Attacks/attack_base.gd
+++ Scripts/Components/Attacks/attack_base.gd
@@ -14,7 +14,19 @@ class_name Attack
 # Runtime variables
 ## Use this to denote when you can take the action
 var can_use: bool = true
+## Timer node used for cooldown
+var cooldown_timer: Timer = null
 
+func _ready():
+	# Make a timer
+	cooldown_timer = Timer.new()
+	# Add as a child
+	add_child(cooldown_timer)
+	# Connnect the signal to renable can_use
+	cooldown_timer.timeout.connect(_on_cooldown_timer_timeout)
+	# Stop the timer from looping
+	cooldown_timer.one_shot = true
+	
 ## Template function for children
 func use():
 	print("Use not yet implemented in this attack")
@@ -22,12 +34,8 @@ func use():
 ## Function to start the cooldown timer.
 ## [br]Optional parameter to overide the cooldown time
 func start_cooldown_timer(time: float = cooldown_time) -> void:
-	# Make a timer
-	var timer = Timer.new()
-	# Connnect the signal to renable can_use
-	timer.timeout.connect(_on_cooldown_timer_timeout)
 	# Start the timer
-	timer.start(time)
+	cooldown_timer.start(time)
 
 func _on_cooldown_timer_timeout() -> void:
 	can_use = true
