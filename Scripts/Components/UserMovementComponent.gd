983658ce4439aa409b73a968a19d8374d383056d
Lee-Roy <ltd1999@hotmail.co.uk>
2024-06-18 09:54:28 +0100

    Fixed a print statement in MoveComponent
    It was printing parent not found when it shouldn't

--- Scripts/Components/UserMovementComponent.gd
+++ Scripts/Components/UserMovementComponent.gd
@@ -17,6 +17,10 @@ func _ready():
 	parent_to_move = get_parent()
 
 func _physics_process(delta):
+	if not parent_to_move:
+		print("Parent not found for movement component")
+		return
+	
 	horizontal = Input.get_axis("move_left","move_right")
 	vertical = Input.get_axis("move_up", "move_down")
 	
@@ -29,9 +33,6 @@ func _physics_process(delta):
 		parent_to_move.velocity = parent_to_move.velocity.move_toward(Vector2(speed*horizontal,speed*vertical),celMoveSpeed*delta)
 		
 		#print(parent_to_move.velocity)
-		
-	else:
-		print("Parent not found for movement component")
 
 
 func set_can_move(flag: bool) -> void:
