a3968bcce70fc39928f5a9489874e39ab4270ead
Lee-Roy <ltd1999@hotmail.co.uk>
2024-06-18 10:16:52 +0100

    Added the ability to change the spawn range.
    You can now place two markers to denote where the range of respawning can take place for the x.

--- Scripts/Level/offscreen_respawner.gd
+++ Scripts/Level/offscreen_respawner.gd
@@ -1,21 +1,24 @@
 extends Area2D
 
-## Area to reset any objects that collide to the top of the screen
+## Area to reset any objects that collide to the top of the screen.
+
 
 #region Object References
-## Gets the viewport width
-@onready var viewport_width: int = get_viewport().size.x
+## Starting marker.
+@onready var start_marker: Marker2D = $StartMarker
+## Ending marker
+@onready var end_marker: Marker2D = $EndMarker
 #endregion
 
 func _on_body_entered(body):
 	if body is Node2D:
 		body.global_position.y = -100
 		# Set the x to be randomly within the width of the viewport
-		body.global_position.x = randi_range(0, viewport_width)
+		body.global_position.x = randi_range(start_marker.global_position.x, end_marker.global_position.x)
 
 
 func _on_area_entered(area):
 	if area is Node2D:
 		area.global_position.y = -100
 		# Set the x to be randomly within the width of the viewport
-		area.global_position.x = randi_range(0, viewport_width)
+		area.global_position.x = randi_range(start_marker.global_position.x, end_marker.global_position.x)
