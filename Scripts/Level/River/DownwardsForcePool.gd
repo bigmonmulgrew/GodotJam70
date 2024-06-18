6b7e4e68b7b5210e444bee0ba4eb0976f008d45e
Lee-Roy <ltd1999@hotmail.co.uk>
2024-06-18 09:44:32 +0100

    Created a downwards force pool.
    Anything that is a child of the DownwardsForcePool will move down at a rate set in the editor.
    Make this match the scrolling background rate to look good.

---
+++ Scripts/Level/River/DownwardsForcePool.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+## The rate at which the objects fall
+@export var gravity: float = 1000
+
+## Array of children in the pool
+var children: Array = []
+
+func _ready():
+	# Get the children
+	children = get_children()
+	
+	
+func _physics_process(delta):
+	for object in children:
+		object.global_position.y += gravity * delta
