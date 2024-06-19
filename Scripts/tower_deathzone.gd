53822ee00925bf63dba2ef0fbcd016fd169206f8
MutantGinga <haydenwilliamhefford@yahoo.com>
2024-06-19 11:03:04 +0100

    Created Tower Area with a deathzone
    Created a basic tower area with a deathzone script to control what happens when you leave the tower (fall).
    
    Fall currently just makes you lose health. Respawning needs more consideration and possibly changes to other scripts, so not implemented here.

---
+++ Scripts/tower_deathzone.gd
@@ -0,0 +1,32 @@
+extends Area2D
+
+var health_component
+@export var respawn_time: float = 3
+var respawn_timer: Timer = null
+
+func start_respawn_timer(time: float = respawn_time) -> void:
+	# Check if timer is null and create first.
+	if respawn_timer == null: create_timer()
+	
+	# Start the timer
+	respawn_timer.start(time)
+
+func _on_respawn_timer_timeout() -> void:
+	print("I HAVE RESPAWNED")
+	#b_is_active = true
+	
+func create_timer():
+	# Make a timer
+	respawn_timer = Timer.new()
+	# Add as a child
+	add_child(respawn_timer)
+	# Connnect the signal to renable can_use
+	respawn_timer.timeout.connect(_on_respawn_timer_timeout)
+	# Stop the timer from looping
+	respawn_timer.one_shot = true
+
+func _on_body_exited(body):
+	health_component = body.get_node("HealthComponent")
+	if health_component:
+		health_component.remove_health(50) # damage type defaults to physical
+		start_respawn_timer(respawn_time)
