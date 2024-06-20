2e0f1d48090342c4b48ebb05171c1a9608bea992
Lee-Roy <ltd1999@hotmail.co.uk>
2024-06-20 12:45:05 +0100

    get_player_location added.
    Added a function to return the active player.

--- Scripts/Character/Boss/boss_base.gd
+++ Scripts/Character/Boss/boss_base.gd
@@ -2,20 +2,55 @@
 extends CharacterBody2D
 class_name Boss
 
-#region Export Variables
+## Base boss script to inherit from.
+## [br]
+## Needs a health component to be added.
+## [br]Needs two Node2D added that are called "MeleeActions" and "RangedActions".
+
+#region Object References
 ## Health component for the boss.
 var health_component: HealthComponent:
 	get:
-		return get_node("HealthComponent") # Tries to find the HealthComponent
+		return get_node("HealthComponent") # Tries to find the HealthComponent.
 	set(value):
-		health_component = value # Set the health component if found
+		health_component = value # Set the health component if found.
+		if Engine.is_editor_hint():
+			update_configuration_warnings()
+
+var melee_actions: Node2D:
+	get:
+		return get_node("MeleeActions") # Tries to find a node2d called MeleeActions.
+	set(value):
+		melee_actions = value # Set the melee actions node if found.
+		if Engine.is_editor_hint():
+			update_configuration_warnings()
+
+var ranged_actions: Node2D:
+	get:
+		return get_node("RangedActions") # Tries to find a node2d called RangedActions.
+	set(value):
+		ranged_actions = value # Set the melee actions node if found.
 		if Engine.is_editor_hint():
 			update_configuration_warnings()
 #endregion
 
-# Inform the user that a health component needs to be added
+#region Runtime Variables
+var player: Player
+#endregion
+
+## Gets the active character from the gameplay manager
+func get_player_location() -> Player:
+	return GameManager.selected_characters[GameManager.active_character_index]
+
+# Inform the user that a health component needs to be added.
 func _get_configuration_warnings() -> PackedStringArray:
-	# If no health component is found, add a warning
+	# If no health component is found, add a warning.
 	if health_component == null:
-		return ["No health component added."]
+		return ["No HealthComponent node found."]
+	# If no melee actions node is found, add a warning.
+	if melee_actions == null:
+		return ["No MeleeActions node found."]
+	# If no ranged actions node is found, add a warning.
+	if ranged_actions == null:
+		return ["No RangedActions node found."]
 	return []
