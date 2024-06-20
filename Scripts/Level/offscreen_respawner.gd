91dd3b998d3ba78de23c27f32cc1c71578e3de60
Lee-Roy <ltd1999@hotmail.co.uk>
2024-06-20 09:26:12 +0100

    Offscreen spawner more adjustable
    Offscreen spawner now uses the y of the start marker.

--- Scripts/Level/offscreen_respawner.gd
+++ Scripts/Level/offscreen_respawner.gd
@@ -1,7 +1,8 @@
 extends Area2D
 
 ## Area to reset any objects that collide to the top of the screen.
-
+## [br]
+## Place the start and end markers to denote the line where objects can respawn
 
 #region Object References
 ## Starting marker.
@@ -12,13 +13,13 @@ extends Area2D
 
 func _on_body_entered(body):
 	if body is Node2D:
-		body.global_position.y = -100
+		body.global_position.y = start_marker.global_position.y
 		# Set the x to be randomly within the width of the viewport
 		body.global_position.x = randi_range(start_marker.global_position.x, end_marker.global_position.x)
 
 
 func _on_area_entered(area):
 	if area is Node2D:
-		area.global_position.y = -100
+		area.global_position.y = start_marker.global_position.y
 		# Set the x to be randomly within the width of the viewport
 		area.global_position.x = randi_range(start_marker.global_position.x, end_marker.global_position.x)
