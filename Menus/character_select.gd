fd08c3de1bf44426b5dd2e5903be765b15278ce3
Stondonk <jastical66@gmail.com>
2024-06-22 22:52:42 +0100

    dogey but functional menu fixes

--- Menus/character_select.gd
+++ Menus/character_select.gd
@@ -2,11 +2,21 @@ extends Control
 
 @export_file("*.tscn") var target_Level_level_1
 @export_file("*.tscn") var target_Level_main_menu
+
+@export var color_button_select:Color = Color(0,0,0.8,0.75)
+@export var color_button_default:Color = Color(0,0,0,0.75)
+
 var game_manager
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	game_manager = get_tree().get_root().get_node("GameManager")
+	var stylebox = StyleBoxFlat.new()
+	stylebox.bg_color = color_button_default
+	for but in get_children():
+		var is_but = but as Button
+		if is_but != null:
+			is_but.add_theme_stylebox_override("normal", stylebox)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -24,12 +34,22 @@ func _on_back_button_pressed() -> void:
 	LevelMaster.load_level(target_Level_main_menu)
 	#get_tree().change_scene_to_file(target_Level_main_menu)
 
-func _character_pass(name:String, nodeP:NodePath):
-	var active:bool = game_manager.character_select_option(name)
-	var ButtonRef =	get_tree().root.get_node(nodeP)
-	print(ButtonRef)
-	if active: ButtonRef.add_theme_color_override("normal",Color(1,0,0,1))
-	else: ButtonRef.add_theme_color_override("normal",Color(0,0,0,1))
+func _character_pass(name: String, ButtonRef: Button):
+	var active: bool = game_manager.character_select_option(name)
 
-func _get_p_my_g(nodeP:NodePath):
-	print(nodeP)
+	var stylebox = StyleBoxFlat.new()
+	if active:
+		stylebox.bg_color = color_button_select # Red color
+	else:
+		stylebox.bg_color = color_button_default # Yellow color
+
+	ButtonRef.add_theme_stylebox_override("normal", stylebox)
+	ButtonRef.add_theme_stylebox_override("hover", stylebox) # Also change hover state if needed
+	ButtonRef.add_theme_stylebox_override("pressed", stylebox) # Also change pressed state if needed
+
+#super dirty but tis the godot method
+
+func _on_select_character_1_button_pressed() -> void:
+	_character_pass("King Arthur", $Select_Character1_Button)
+func _on_select_character_2_button_pressed() -> void:
+	_character_pass("Robin Hood", $Select_Character2_Button)
