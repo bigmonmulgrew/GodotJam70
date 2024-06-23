68aad85e1dcee8efc98fef8d3e3ede26e47e684d
Lee-Roy <ltd1999@hotmail.co.uk>
2024-06-23 09:49:37 +0100

    Changed state logic

--- Scripts/Character/Boss/merlin.gd
+++ Scripts/Character/Boss/merlin.gd
@@ -8,31 +8,37 @@ enum State{ WALK_N, WALK_NE, WALK_E, WALK_SE, WALK_S, WALK_SW, WALK_W, WALK_NW }
 
 #region Runtime Variables
 var current_state: State = State.WALK_S
+var speed: float = 40
+var anim_move_tolerance: float = 0.1
 #endregion
 
 func _physics_process(delta):
 	super(delta)
 	_update_state()
 	_process_state()
+	_test_movement()
+	move_and_slide()
 	
 func _update_state():
-	match velocity.normalized():
-		Vector2.UP:
-			current_state = State.WALK_N
-		Vector2.RIGHT:
-			current_state = State.WALK_E
-		Vector2.DOWN:
-			current_state = State.WALK_S
-		Vector2.LEFT:
-			current_state = State.WALK_E
-		Vector2(1, -1):
-			current_state = State.WALK_NE
-		Vector2(1, 1):
-			current_state = State.WALK_SE
-		Vector2(-1, 1):
-			current_state = State.WALK_SW
-		Vector2(-1, -1):
-			current_state = State.WALK_NW
+	var norm_velocity = velocity.normalized()
+	print(norm_velocity)
+	# Check walk north east
+	if norm_velocity.x > anim_move_tolerance and norm_velocity.y < anim_move_tolerance:
+		current_state = State.WALK_NE
+	# Check walk south east
+	elif norm_velocity.x > anim_move_tolerance and norm_velocity.y > -anim_move_tolerance:
+		current_state = State.WALK_SE
+	elif (norm_velocity.x < anim_move_tolerance or norm_velocity.x > -anim_move_tolerance) and norm_velocity.y < 0: 
+		current_state = State.WALK_N
+	# Check walk east
+	elif norm_velocity.x > 0 and (norm_velocity.y < anim_move_tolerance or norm_velocity.y > -anim_move_tolerance): 
+		current_state = State.WALK_E
+	# Check walk south
+	elif (norm_velocity.x < anim_move_tolerance or norm_velocity.x > -anim_move_tolerance) and norm_velocity.y > 0: 
+		current_state = State.WALK_S
+	# Check walk west
+	elif norm_velocity.x < 0 and (norm_velocity.y < anim_move_tolerance or norm_velocity.y > -anim_move_tolerance): 
+		current_state = State.WALK_W
 
 func _process_state():
 	match current_state:
@@ -52,3 +58,8 @@ func _process_state():
 			animated_sprite.play("WalkSW")
 		State.WALK_NW:
 			animated_sprite.play("WalkNW")
+			
+	print(current_state)
+
+func _test_movement():
+	velocity = global_position.direction_to(get_player_location()) * speed
